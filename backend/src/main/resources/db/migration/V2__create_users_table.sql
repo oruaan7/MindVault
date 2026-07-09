90838d6e8cade7ffd3629835901f1cb6ba016bba
ruan souza <contatooruaan@outlook.com>
2026-07-09 18:30:37 -0300

    feat(database): create users table migration

---
+++ backend/src/main/resources/db/migration/V2__create_users_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE users (
+                       id UUID PRIMARY KEY,
+
+                       name VARCHAR(150) NOT NULL,
+
+                       email VARCHAR(255) NOT NULL UNIQUE,
+
+                       password VARCHAR(255) NOT NULL,
+
+                       created_at TIMESTAMP NOT NULL,
+
+                       updated_at TIMESTAMP NOT NULL
+);
