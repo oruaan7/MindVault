0c070585f311b1ff4a0b20bc1372afe9ddde1482
ruan souza <contatooruaan@outlook.com>
2026-07-09 18:40:20 -0300

    feat(user): changed user entity

--- backend/src/main/java/com/mindvault/user/entity/User.java
+++ backend/src/main/java/com/mindvault/user/entity/User.java
@@ -1,4 +1,27 @@
 package com.mindvault.user.entity;
 
-public class User {
+import com.mindvault.common.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity {
+
+    @Column(nullable = false, length = 150)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 255)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
 }
