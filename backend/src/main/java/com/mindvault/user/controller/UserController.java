4c9ddd17a424e554b89833a2296ee4a5f57726ff
ruan souza <contatooruaan@outlook.com>
2026-07-10 12:11:24 -0300

    feat(user): implementing user controller

---
+++ backend/src/main/java/com/mindvault/user/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.mindvault.user.controller;
+
+import com.mindvault.user.dto.CreateUserRequest;
+import com.mindvault.user.dto.UserResponse;
+import com.mindvault.user.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserResponse create(@Valid @RequestBody CreateUserRequest request) {
+        return userService.create(request);
+    }
+
+}
