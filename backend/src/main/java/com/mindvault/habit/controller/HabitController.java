926d13a9193e78864fb2491f1debc5ec1f6599fd
ruan souza <contatooruaan@outlook.com>
2026-07-11 20:18:31 -0300

    feat(habit): create habits controller

--- backend/src/main/java/com/mindvault/habit/controller/HabitController.java
+++ backend/src/main/java/com/mindvault/habit/controller/HabitController.java
@@ -1,4 +1,29 @@
 package com.mindvault.habit.controller;
 
+import com.mindvault.habit.dto.CreateHabitRequest;
+import com.mindvault.habit.dto.HabitResponse;
+import com.mindvault.habit.service.HabitService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/habits")
+@RequiredArgsConstructor
 public class HabitController {
+
+    private final HabitService habitService;
+
+    @PostMapping
+    public HabitResponse create(
+        @Valid @RequestBody CreateHabitRequest request,
+        Authentication authentication
+    ) {
+
+        return habitService.create(
+            request,
+            authentication.getName()
+        );
+    }
 }
--- backend/src/main/java/com/mindvault/habit/service/HabitService.java
+++ backend/src/main/java/com/mindvault/habit/service/HabitService.java
@@ -1,4 +1,38 @@
 package com.mindvault.habit.service;
 
+import com.mindvault.habit.dto.CreateHabitRequest;
+import com.mindvault.habit.dto.HabitResponse;
+import com.mindvault.habit.entity.Habit;
+import com.mindvault.habit.repository.HabitRepository;
+import com.mindvault.user.entity.User;
+import com.mindvault.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class HabitService {
+
+    private final HabitRepository habitRepository;
+    private final UserRepository userRepository;
+
+    public HabitResponse create(CreateHabitRequest request, String email) {
+
+        User user = userRepository.findByEmail(email)
+            .orElseThrow(() -> new IllegalArgumentException("User not found"));
+
+        Habit habit = new Habit();
+        habit.setTitle(request.title());
+        habit.setDescription(request.description());
+        habit.setUser(user);
+
+        Habit savedHabit = habitRepository.save(habit);
+
+        return new HabitResponse(
+            savedHabit.getId(),
+            savedHabit.getTitle(),
+            savedHabit.getDescription(),
+            savedHabit.isActive()
+        );
+    }
 }
