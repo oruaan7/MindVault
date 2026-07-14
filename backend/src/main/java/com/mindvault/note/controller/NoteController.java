a2232898d2bebafbf67f2c5ef6de8101dbda3274
ruan souza <contatooruaan@outlook.com>
2026-07-14 09:39:00 -0300

    feat(note): implement create note endpoint

--- backend/src/main/java/com/mindvault/note/controller/NoteController.java
+++ backend/src/main/java/com/mindvault/note/controller/NoteController.java
@@ -1,12 +1,33 @@
 package com.mindvault.note.controller;
 
+import com.mindvault.note.dto.CreateNoteRequest;
+import com.mindvault.note.dto.NoteResponse;
+import com.mindvault.note.service.NoteService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/notes")
 @RequiredArgsConstructor
 public class NoteController {
+
+    private final NoteService noteService;
+
+    @PostMapping
+    public NoteResponse create(
+        @Valid
+        @RequestBody
+        CreateNoteRequest request,
+        Authentication authentication
+    ) {
+
+        return noteService.create(
+            request,
+            authentication.getName()
+        );
+
+    }
 
 }
--- backend/src/main/java/com/mindvault/note/service/NoteService.java
+++ backend/src/main/java/com/mindvault/note/service/NoteService.java
@@ -1,10 +1,48 @@
 package com.mindvault.note.service;
 
+import com.mindvault.note.dto.CreateNoteRequest;
+import com.mindvault.note.dto.NoteResponse;
+import com.mindvault.note.entity.Note;
+import com.mindvault.note.repository.NoteRepository;
+import com.mindvault.user.entity.User;
+import com.mindvault.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class NoteService {
+
+    private final NoteRepository noteRepository;
+    private final UserRepository userRepository;
+
+    public NoteResponse create(
+        CreateNoteRequest request,
+        String email
+    ) {
+
+        User user = userRepository.findByEmail(email)
+            .orElseThrow(() ->
+                new IllegalArgumentException("User not found"));
+
+        Note note = new Note();
+
+        note.setTitle(request.title());
+        note.setContent(request.content());
+        note.setFavorite(false);
+        note.setArchived(false);
+        note.setUser(user);
+
+        Note savedNote = noteRepository.save(note);
+
+        return new NoteResponse(
+            savedNote.getId(),
+            savedNote.getTitle(),
+            savedNote.getContent(),
+            savedNote.isFavorite(),
+            savedNote.isArchived()
+        );
+
+    }
 
 }
