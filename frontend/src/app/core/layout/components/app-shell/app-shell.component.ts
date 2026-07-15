f9e72b1ffea09f4abf7472a9c79aea5a2fa893b0
ruan souza <contatooruaan@outlook.com>
2026-07-15 16:41:05 -0300

    feat(frontend): create app shell components

---
+++ frontend/src/app/core/layout/components/app-shell/app-shell.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { HeaderComponent } from '../header/header.component';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import { FooterComponent } from '../footer/footer.component';
+
+@Component({
+
+    selector:'app-shell',
+
+    standalone:true,
+
+    imports:[
+        HeaderComponent,
+        SidebarComponent,
+        FooterComponent,
+        RouterOutlet
+    ],
+
+    templateUrl:'./app-shell.component.html',
+
+    styleUrl:'./app-shell.component.scss'
+
+})
+export class AppShellComponent{}
