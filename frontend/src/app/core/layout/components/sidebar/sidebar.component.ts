bd1ac9c0c44c138fb98c9b254ec2280b6ab50c2c
ruan souza <contatooruaan@outlook.com>
2026-07-16 12:45:25 -0300

    feat(layout): redesign app sidebar

--- frontend/src/app/core/layout/components/sidebar/sidebar.component.ts
+++ frontend/src/app/core/layout/components/sidebar/sidebar.component.ts
@@ -1,11 +1,51 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
+  imports: [
+    CommonModule,
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
+
+  readonly items = [
+
+    {
+      label: 'Dashboard',
+      route: '/dashboard'
+    },
+    {
+      label: 'Hábitos',
+      route: '/habits'
+    },
+    {
+      label: 'Metas',
+      route: '/goals'
+    },
+    {
+      label: 'Projetos',
+      route: '/projects'
+    },
+    {
+      label: 'Notas',
+      route: '/notes'
+    },
+    {
+      label: 'Finanças',
+      route: '/finance'
+    },
+    {
+      label: 'Analytics',
+      route: '/analytics'
+    }
+
+  ];
 
 }
