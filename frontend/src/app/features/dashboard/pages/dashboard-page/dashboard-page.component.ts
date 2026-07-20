a31ce0929b57055b55add9b8e155352cf26bfa01
oruaan-PC <contatooruaan@outlook.com>
2026-07-20 10:10:19 -0300

    feat(dashboard): create dashboard page

---
+++ frontend/src/app/features/dashboard/pages/dashboard-page/dashboard-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mv-dashboard-page',
+  standalone: true,
+  templateUrl: './dashboard-page.component.html',
+  styleUrl: './dashboard-page.component.scss'
+})
+export class DashboardPageComponent {}
