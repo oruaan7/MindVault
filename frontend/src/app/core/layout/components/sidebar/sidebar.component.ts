01e5dfe2cb1854a8d3218a90435b1a8165936acb
oruaan-PC <contatooruaan@outlook.com>
2026-07-20 10:04:38 -0300

    feat(layout): implement sidebar v1 with navigation icons

--- frontend/src/app/core/layout/components/sidebar/sidebar.component.ts
+++ frontend/src/app/core/layout/components/sidebar/sidebar.component.ts
@@ -1,14 +1,17 @@
 import { Component, inject } from '@angular/core';
 
 import { RouterLink } from '@angular/router';
-
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLinkActive } from '@angular/router';
 import { NavigationService } from '../../services/navigation.service';
 
 @Component({
   selector: 'mv-sidebar',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    RouterLinkActive,
+    MatIconModule
   ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
