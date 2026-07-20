31eb87b0a7deefa03a856d19b918444485416b13
oruaan-PC <contatooruaan@outlook.com>
2026-07-20 09:21:43 -0300

    feat(layout): create sidebar component

---
+++ frontend/src/app/core/layout/components/sidebar/sidebar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mv-sidebar',
+  standalone: true,
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.scss'
+})
+export class SidebarComponent {}
